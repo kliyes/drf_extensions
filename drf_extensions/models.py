2f534eacee375b851b0b54ba1e1e7f46090eb980
kliyes <tom@kliyes.com>
2019-04-13 15:44:22 +0800

    add ExpirationToken model

---
+++ drf_extensions/models.py
@@ -0,0 +1,22 @@
+import datetime
+
+from django.conf import settings
+
+from rest_framework.authtoken.models import Token
+
+
+class ExpirationToken(Token):
+
+    @property
+    def expired_at(self):
+        time_delta = datetime.timedelta(seconds=settings.AUTH_TOKEN_EXPIRED_IN)
+        return self.created + time_delta
+
+    @property
+    def expired(self):
+        if settings.DEBUG:
+            return False
+        return self.expired_at < timezone.now()
+
+    class Meta(Token.Meta):
+        abstract = False
