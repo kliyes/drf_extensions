d77ddda4be110e8cc26b5b8de5d16d3e0717ae80
kliyes <tom@kliyes.com>
2019-04-13 22:52:32 +0800

    update serializers

--- drf_extensions/serializers.py
+++ drf_extensions/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 
 from .fields import TZDateTimeField
+from .models import ExpirationToken
 
 
 class BaseModelSerializer(serializers.ModelSerializer):
     created_at = TZDateTimeField(read_only=True, format="%Y-%m-%d %H:%M:%S")
     updated_at = TZDateTimeField(read_only=True, format="%Y-%m-%d %H:%M:%S")
+
+
+class ExpirationTokenSerializer(serializers.ModelSerializer):
+    expired_at = TZDateTimeField(read_only=True, format="%Y-%m-%d %H:%M:%S")
+    token = serializers.ReadOnlyField(source="key")
+
+    class Meta:
+        model = ExpirationToken
+        exclude = ("created", "key")
